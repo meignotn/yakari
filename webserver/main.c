065b2fed0d33fa424079d80c622565a89d72c718
’Nicolas <nicolas1.meignotte@etudiant.univ-lille1.fr>
2015-01-20 16:34:58 +0100

    tp termine

--- webserver/main.c
+++ webserver/main.c
@@ -15,21 +15,23 @@ int main(){
 	printf("hello world");
 	int socket_serveur =creer_serveur(8000);
 	int socket_client ;
-	char * buff=malloc(50);
-	printf("test");
 	
 	while(1){
 		socket_client = accept ( socket_serveur , NULL , NULL );
 		if ( socket_client == -1)
 		{
-			perror ( " accept " );
+			perror ( "accept" );
 			/* traitement d ’ erreur */
 		}	
-			const char * message_bienvenue = " Bonjour , bienvenue sur mon serveur \n****************\n*    YAKARI    *\n****************\nThe ultimate web server\nall your bases are belong to us\n check this amazing tribal dance\n\n     X \n____/ \\__o____ ┗(°.°)┓\n   /`-'\\ T\n. /   ) \\   o  ┗(°.°)┛\n /`--/--'\\ /\\ \n:`-./A_,-'/> U ┏(°.°)┛"  ;
+			const char * message_bienvenue = "Bonjour , bienvenue sur mon serveur \n****************\n*    YAKARI    *\n****************\nThe ultimate web server\nall your bases are belong to us\nCheck this amazing tribal dance\n\n     X \n____/ \\__o____ ┗(°.°)┓\n   /`-'\\ T\n. /   ) \\   o  ┗(°.°)┛\n /`--/--'\\ /\\ \n:`-./A_,-'/> U ┏(°.°)┛"  ;
 			write(socket_client , message_bienvenue , strlen(message_bienvenue));
 			while(1==1){
-				read(socket_client,buff,50);
-				write(socket_client , buff , 50);
+				char buff[50];
+				int taille=read(socket_client,buff,50);
+				if(taille==0 || taille ==-1){
+					break;				
+				}
+				write(socket_client , buff , taille);
 			}
 		}
 	
