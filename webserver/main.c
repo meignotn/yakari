2259d8da10564048cec6f89ac40853091cef6b9a
’Nicolas <nicolas1.meignotte@etudiant.univ-lille1.fr>
2015-01-26 14:42:31 +0100

    REUSEADDR

--- webserver/main.c
+++ webserver/main.c
@@ -10,12 +10,37 @@
 #include <arpa/inet.h>
 #include <unistd.h> 
 #include <netdb.h> 
+
+void afficher_message(int socket_client ){
+	const char * message_bienvenue = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nBonjour , bienvenue sur mon serveur \n****************\n*    YAKARI    *\n****************\nThe ultimate web server\nall your bases are belong to us\nCheck this amazing tribal dance\n\n     X \n____/ \\__o____ \n   /`-'\\ T      ___\n. /   ) \\   o ┗(°.°)┛\n /`--/--'\\ /\\   ( )	 \n:`-./A_,-'/> \\  / \\\n"  ;  
+			write(socket_client , message_bienvenue , strlen(message_bienvenue));
+			sleep(1);
+			message_bienvenue = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nBonjour , bienvenue sur mon serveur \n****************\n*    YAKARI    *\n****************\nThe ultimate web server\nall your bases are belong to us\nCheck this amazing tribal dance\n\n     X \n____/ \\__o____ \n   /`-'\\ T      ___\n. /   ) \\   o ┗(°.°)┓\n /`--/--'\\ /\\   ( )	 \n:`-./A_,-'/> \\  / \\\n"  ;
+			write(socket_client , message_bienvenue , strlen(message_bienvenue));
+			sleep(1);
+			message_bienvenue = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nBonjour , bienvenue sur mon serveur \n****************\n*    YAKARI    *\n****************\nThe ultimate web server\nall your bases are belong to us\nCheck this amazing tribal dance\n\n     X \n____/ \\__o____ \n   /`-'\\ T      ___\n. /   ) \\   o ┏(°.°)┛\n /`--/--'\\ /\\   ( )	 \n:`-./A_,-'/> \\  / \\\n" ;
+			write(socket_client , message_bienvenue , strlen(message_bienvenue));
+			sleep(1);
+			message_bienvenue = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nBonjour , bienvenue sur mon serveur \n****************\n*    YAKARI    *\n****************\nThe ultimate web server\nall your bases are belong to us\nCheck this amazing tribal dance\n\n     X \n____/ \\__o____ \n   /`-'\\ T      ___\n. /   ) \\   o ┗(°.°)┓\n /`--/--'\\ /\\   ( )	 \n:`-./A_,-'/> \\  / \\\n"  ; 
+			write(socket_client , message_bienvenue , strlen(message_bienvenue));
+			sleep(1);
+			message_bienvenue = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nBonjour , bienvenue sur mon serveur \n****************\n*    YAKARI    *\n****************\nThe ultimate web server\nall your bases are belong to us\nCheck this amazing tribal dance\n\n     X \n____/ \\__o____ \n   /`-'\\ T      ___\n. /   ) \\   o ┏(°.°)┛\n /`--/--'\\ /\\   ( )	 \n:`-./A_,-'/> \\  / \\\n" ;
+			write(socket_client , message_bienvenue , strlen(message_bienvenue));
+			sleep(1);
+			message_bienvenue = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nBonjour , bienvenue sur mon serveur \n****************\n*    YAKARI    *\n****************\nThe ultimate web server\nall your bases are belong to us\nCheck this amazing tribal dance\n\n     X \n____/ \\__o____ \n   /`-'\\ T      ___\n. /   ) \\   o ┗(°.°)┛\n /`--/--'\\ /\\   ( )	 \n:`-./A_,-'/> \\  / \\\n"  ;
+			write(socket_client , message_bienvenue , strlen(message_bienvenue));
+			
+}
+
+
 int main(){
 	fflush(stdout);
 	printf("hello world");
 	int socket_serveur =creer_serveur(8000);
 	int socket_client ;
-	
+	/*int bool=1;
+	unsigned int i;
+	char dance[19]="do this dance again";*/	
 	while(1){
 		socket_client = accept ( socket_serveur , NULL , NULL );
 		if ( socket_client == -1)
@@ -23,14 +48,25 @@ int main(){
 			perror ( "accept" );
 			/* traitement d ’ erreur */
 		}	
-			const char * message_bienvenue = "Bonjour , bienvenue sur mon serveur \n****************\n*    YAKARI    *\n****************\nThe ultimate web server\nall your bases are belong to us\nCheck this amazing tribal dance\n\n     X \n____/ \\__o____ ┗(°.°)┓\n   /`-'\\ T\n. /   ) \\   o  ┗(°.°)┛\n /`--/--'\\ /\\ \n:`-./A_,-'/> U ┏(°.°)┛"  ;
-			write(socket_client , message_bienvenue , strlen(message_bienvenue));
-			while(1==1){
+			afficher_message(socket_client);
+			while(1){
 				char buff[50];
 				int taille=read(socket_client,buff,50);
 				if(taille==0 || taille ==-1){
 					break;				
 				}
+				
+				/*for(i=0;i<10;i++){
+					if(buff[i]!=dance[i]){
+						bool = 0;						
+						break ;
+					}
+				}
+				if(bool){
+					afficher_message(socket_client);		
+				}*/
+			
+
 				write(socket_client , buff , taille);
 			}
 		}
--- webserver/socket.c
+++ webserver/socket.c
@@ -26,10 +26,13 @@ int creer_serveur(int port){
 	struct sockaddr_in saddr;
 	saddr.sin_family=AF_INET;/* Socket ipv4 */
 	saddr.sin_port = htons (port); /* Port d ’ écoute */
-	saddr.sin_addr.s_addr = INADDR_ANY ; /* écoute sur toutes les interfaces */	
+	saddr.sin_addr.s_addr = INADDR_ANY ; /* écoute sur toutes les interfaces */
+	int optval = 1;
+	if ( setsockopt ( socket_serveur , SOL_SOCKET , SO_REUSEADDR , & optval , sizeof(int)) == -1)
+		perror ( " Can not set SO_REUSEADDR option " );	
 	if (bind(socket_serveur,(struct sockaddr *)& saddr , sizeof( saddr )) == -1)
 	{
-		perror ( " bind socker_serveur " );
+		perror ( " bind socket_serveur " );
 		/* traitement de l ’ erreur */
 	}
 	if ( listen ( socket_serveur , 10) == -1)
@@ -38,5 +41,8 @@ int creer_serveur(int port){
 		/* traitement d ’ erreur */
 	}
 	
+	
+	
 	return socket_serveur;
 }
+
