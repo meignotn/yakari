12cf40252d81b6bba21a06b38edd0a75cd7ca7e1
’Nicolas <nicolas1.meignotte@etudiant.univ-lille1.fr>
2015-02-09 14:06:13 +0100

    simple_get

--- webserver/main.c
+++ webserver/main.c
@@ -38,7 +38,7 @@ int main(void){
 	initialiser_signaux();
 	int socket_serveur =creer_serveur(8000);
 	int socket_client ;
-	
+	int valide=0;
 	while(1){
 		socket_client = accept ( socket_serveur , NULL , NULL );
 		if (socket_client == -1)
@@ -51,27 +51,26 @@ int main(void){
 			close(socket_client);
 		}else{
 			FILE *fichier_client= fdopen(socket_client,"w+");			/*FILE *fichier_serveur= fdopen(socket_serveur,"w+");			*/
-			afficher_message(socket_client);
-			int ignore=0;
-			while(1){		
-				char buff[8000];
+			char buff[8000];
+			if(fgets(buff,8000,fichier_client)==NULL){
+				break;
+			}if(requetevalide(buff)){
+				valide=1;
+				printf("%s",buff);
+			}
+			int taille = strlen(buff);
+			while(!(buff[0]=='\n' || (buff[0]=='\r' && buff[1]=='\n'))){
 				if(fgets(buff,8000,fichier_client)==NULL){
 					break;
-				}if(ignore==0){
-					if(buff[0]=='\n' || (buff[0]=='\r' && buff[1]=='\n')){
-						ignore=1;	
-					}
 				}
-				if(ignore){
-					int taille = strlen(buff);
-					if(requetevalide(buff)){
-						printf("%s",buff);
-						fprintf(fichier_client, "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: %d\r\n",taille );
-					}else{
-						fprintf(fichier_client, "HTTP/1.1 400 BAD REQUEST\r\nConnection: close\r\nContent-Length:%d\r\n",taille  );
-					}
-				}
-
+				taille=taille+strlen(buff);
+			}
+			if(valide){
+				afficher_message(socket_client);
+				fprintf(fichier_client, "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: %d\n\n200 OK\r\n",taille );
+				
+			}else{
+				fprintf(fichier_client, "HTTP/1.1 400 BAD REQUEST\r\nConnection: close\r\nContent-Length:%d\n\n400 Bad request\r\n",taille  );
 			}
 			exit(0);
 		}
