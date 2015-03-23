1cd6db7bd2ca769fa76dced9a3f9863aa10df728
Leon VICTOR <leon.victor@etudiant.univ-lille1.fr>
2015-03-23 14:28:29 +0100

    USERADDR 8080

--- webserver/main.c
+++ webserver/main.c
@@ -36,7 +36,7 @@ void afficher_message(int socket_client ){
 int main(){
 	fflush(stdout);
 	printf("hello world");
-	int socket_serveur =creer_serveur(8000);
+	int socket_serveur =creer_serveur(8080);
 	int socket_client ;
 	/*int bool=1;
 	unsigned int i;
